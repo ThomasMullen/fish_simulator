7b0bb9ba04144e9d1c10e418a332ad1fe4dcf24b
ThomasMullen <thomasmullen96@gmail.com>
2023-08-27 14:33:58 +0100

    added simulator doc

--- src/zf_animator_tsm/simulator.py
+++ src/zf_animator_tsm/simulator.py
@@ -1,3 +1,5 @@
+"""Module pull all functionality and generates the images and videos
+"""
 import os
 import itertools
 from pathlib import Path
@@ -17,10 +19,10 @@ from .tail_transformation import (
 
 
 def make_simulation(
-    data: np.ndarray,
+    data: NDArray,
     vid_fname: str,
     upsample: int = None,
-    fp: str = None,
+    f_path: str = None,
     keep_pngs: bool = False,
 ):
     """simulate fish dynamics on tail image
@@ -40,17 +42,17 @@ def make_simulation(
 
     """
     assert data.ndim == 2, "Need to be 2D"
-    if fp is None:
+    if f_path is None:
         import tempfile
 
-        fp = tempfile.mkdtemp()
-        print("Tmp dir: {}".format(fp))
+        f_path = tempfile.mkdtemp()
+        print(f"Tmp dir: {f_path}")
     else:
-        fp = Path(fp)
-        fp.mkdir(parents=True, exist_ok=True)
+        f_path = Path(f_path)
+        f_path.mkdir(parents=True, exist_ok=True)
 
     data = data.T if data.shape[0] < data.shape[1] else data
-    tps, n_seg = data.shape
+    tps = data.shape[0]
 
     # load image data
     img_loader = ImageLoader()
@@ -76,7 +78,7 @@ def make_simulation(
     )
     # smooth signals
     intp_x, intp_y = interpolate_keypoints(
-        tail_x, tail_y, n_segments=KeypointStruct(tail_x, tail_y, len(segs) * upsample)
+        KeypointStruct(tail_x, tail_y, len(segs) * upsample)
     )
 
     # convert pos to angle
@@ -90,57 +92,55 @@ def make_simulation(
     # image hyper parameters
     head_y_len, head_x_len = head.shape[:2]
     y_offset = 10
-    sf = 1.0
+    scale_fac = 1.0
 
-    for t_ in trange(body_ang.size):
+    for i_tp in trange(body_ang.size):
         cum_x_len = head_x_len
         # plot the head
-        fig, ax = plt.subplots(dpi=400)
-        rot = transforms.Affine2D().rotate(np.mod(body_ang[t_], 2 * np.pi))
-        ax.imshow(
+        fig, ax_tail = plt.subplots(dpi=400)
+        rot = transforms.Affine2D().rotate(np.mod(body_ang[i_tp], 2 * np.pi))
+        ax_tail.imshow(
             head,
             extent=[0, head_x_len, -head_y_len // 2, head_y_len // 2],
-            # transform=rot  + ax.transData,
-            transform=ax.transData,
+            # transform=rot  + ax_tail.transData,
+            transform=ax_tail.transData,
             alpha=0.8,
         )
         # plot segments
         for i, seg in enumerate(segs):
             # add a single segment to scale
             seg_y_len, seg_x_len = seg.shape[:2]
-            rot = transforms.Affine2D().rotate(np.mod(intp_angs[t_, i], 2 * np.pi))
+            rot = transforms.Affine2D().rotate(np.mod(intp_angs[i_tp, i], 2 * np.pi))
             trs = transforms.Affine2D().translate(tx=cum_x_len, ty=y_offset)
-            ax.imshow(
+            ax_tail.imshow(
                 seg,
                 extent=[0, seg_x_len, -seg_y_len // 2, seg_y_len // 2],
-                transform=trs + rot + ax.transData,
+                transform=trs + rot + ax_tail.transData,
                 alpha=0.8,
             )
             cum_x_len += seg_x_len
-        ax.spines[["left", "right", "top", "bottom"]].set_visible(False)
-        ax.set(
+        ax_tail.spines[["left", "right", "top", "bottom"]].set_visible(False)
+        ax_tail.set(
             yticks=[],
             xticks=[],
             xlim=[0, cum_x_len + 20],
             # ylim=[-head_y_len//2, head_y_len//2],
             # ylim=[-head_y_len*sf, head_y_len*sf],
-            ylim=[(-cum_x_len) * sf, cum_x_len * sf],
+            ylim=[(-cum_x_len) * scale_fac, cum_x_len * scale_fac],
             # ylim=[(-cum_x_len+head_x_len)*sf, cum_x_len-head_x_len*sf],
         )
 
-        fig.savefig(f"{fp}/{t_:03}.png", dpi=150)
+        fig.savefig(f"{f_path}/{i_tp:03}.png", dpi=150)
         plt.close(fig)
 
     if os.path.exists(vid_fname):
         os.remove(vid_fname)
 
     # make video
-    cmd = "ffmpeg -r 35 -f image2 -i {}/%03d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p {}".format(
-        fp, vid_fname
-    )
+    cmd = f"ffmpeg -r 35 -f image2 -i {f_path}/%03d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p {vid_fname}"
     os.system(cmd)
-    print("Saving video to: {}".format(vid_fname))
+    print(f"Saving video to: {vid_fname}")
 
     if not keep_pngs:
-        os.remove(fp)
-        print("delete: {} folder".format(fp))
+        os.remove(f_path)
+        print(f"delete: {f_path} folder")
