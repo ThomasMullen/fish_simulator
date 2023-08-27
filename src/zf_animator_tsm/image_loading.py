0d800130d0bd4e62d226ee4d140ed406bec2afe2
ThomasMullen <thomasmullen96@gmail.com>
2023-08-27 01:38:21 +0100

    pylint image loading mod

--- src/zf_animator_tsm/image_loading.py
+++ src/zf_animator_tsm/image_loading.py
@@ -1,22 +1,30 @@
-import numpy as np
+"""Module loading image for animation."""
 import os
-from PIL import Image
 from pathlib import Path
 from typing import List, Dict
+import numpy as np
 from numpy.typing import NDArray
+from PIL import Image
 
 
 class ImageLoader:
-    def __init__(self, dir=None) -> None:
+    """Self contained loader that returns the tail segments and head images.
+    """
+    def __init__(self, seg_dir=None) -> None:
         self.seg_dir = (
             Path(os.getcwd(), "src", "zf_animator_tsm", "template_img", "segs")
-            if dir is None
-            else dir
+            if seg_dir is None
+            else seg_dir
         )
         self.head_dir = Path(self.seg_dir).parent
         print(self.seg_dir)
 
     def load_segments(self) -> List[NDArray]:
+        """Load list of segment pngs into numpy arrays of different shape
+
+        Returns:
+            List[NDArray]: elements of numpy arrays of different shape forming tail.
+        """
         n_segs = len(list(Path(self.seg_dir).iterdir()))
         segs = [
             np.asarray(Image.open(f"{self.seg_dir}/{i:02}.png")) for i in range(n_segs)
@@ -24,12 +32,22 @@ class ImageLoader:
         return segs
 
     def load_head(self) -> np.ndarray:
+        """Load the head of fish
+
+        Returns:
+            np.ndarray: head 2D image array
+        """
         head = np.asarray(Image.open(f"{self.head_dir}/fish_head.png"))
         return head
 
     def return_zf_img(self) -> Dict:
-        zf_image = dict(
-            head=self.load_head(),
-            segs=self.load_segments(),
-        )
+        """Merge tail and head dataset to a dictionary
+
+        Returns:
+            Dict: keys 'head' and 'segs' composing tail.
+        """
+        zf_image = {
+            "head": self.load_head(), 
+            "segs": self.load_segments()
+            }
         return zf_image
