08e7819c9f527dfe6c26acc045d67ab3f5919254
Thomas <thomasmullen96@gmail.com>
2023-09-12 22:30:34 +0100

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="fish_simulator",
-    version="0.0.9",
+    version="0.0.10",
     packages=find_packages(),
     description="Python toolbox to transform zebrafish\
         tracking data to tail animations",
